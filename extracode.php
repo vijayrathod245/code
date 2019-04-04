d9a1bb4f43088227f5e296f4eab5a30940c66544
Test <46293565+vijayrathod245@users.noreply.github.com>
2019-04-04 12:36:26 +0530

    Update extracode.php

--- extracode.php
+++ extracode.php
@@ -179,3 +179,91 @@ function add_insert_data(){?>
 	</form>
 <?php }
 
+
+/* Perant and child category display */
+
+add_shortcode('show_sub_category_list', 'load_sub_category_function');
+
+function load_sub_category_function() {
+	ob_start();
+	$category = get_queried_object();
+	?>
+	<div class="row">
+	<?php
+		if ($category->parent == 0) {
+			$categories = get_categories( array( 'parent' => $category->cat_ID,'hide_empty' => false ) ); 
+		    foreach ( $categories as $category ) {
+	?>
+		        <div class="col-md-4">
+		        	<a href="<?php echo get_category_link($category->term_id) ?>">
+			        	<div class="category-box">
+				        	<div class="title">
+				        		<?php echo $category->cat_name; ?>
+				        	</div>
+				        	<div class="icon">
+				        		<img src="<?php echo z_taxonomy_image_url($category->term_id); ?>" />
+				        	</div>
+			        	</div>
+		        	</a>
+		        </div>
+    <?php
+		    }
+		} else { 
+			$categories = get_categories( array( 'parent' => $category->parent,'hide_empty' => false ) );
+			?>
+			<div class="col-md-12 mb-4">
+				<div class="category-dropdown">
+					<select onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+						<?php 
+							foreach ($categories as $subcategory):
+								echo '<option '. (($category->cat_ID == $subcategory->term_id) ? 'selected' : '') .' value="'. get_category_link($subcategory->term_id) .'">'. $subcategory->cat_name .'</option>';
+							endforeach;
+						?>
+					</select>
+				</div>
+			</div>
+			<?php
+
+			$posts = get_posts( array('category' => $category->cat_ID) );
+			echo '<div class="post-lists"><ul>';
+			foreach ( $posts as $post ) :
+			?>
+				<li>
+					<a href="<?php the_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a>	
+				</li>
+			<?php
+			endforeach; 
+			echo '</ul></div>';
+		}
+    ?>
+	</div>
+    <?php
+    return ob_get_clean();
+}
+
+add_shortcode('header_category_dropdown', 'load_header_category_function');
+
+function load_header_category_function() {
+	$active_category = get_queried_object();
+	$categories = get_categories( array( 'parent' => 0,'hide_empty' => false, 'orderby' => 'id', 'exclude' => array(1) ) );  
+    ?>
+		<div class="header-category-dropdown">
+			<select onchange="this.options[this.selectedIndex].value && (window.location = this.options[this.selectedIndex].value);">
+				<?php
+					foreach ($categories as $category):
+						echo '<option '. (($active_category->term_id == $category->cat_ID || $active_category->parent == $category->cat_ID) ? 'selected' : '') .' value="'. get_category_link($category->term_id) .'">'. $category->cat_name .'</option>';
+					endforeach;
+				?>
+			</select>
+		</div>
+	<?php
+}
+
+add_action('wp_enqueue_scripts', 'hook_bootstrap');
+
+function hook_bootstrap() {
+	wp_enqueue_style( 'bootstrap-style', get_stylesheet_directory_uri() . '/bootstrap.css' );
+}
+
+?>
+
